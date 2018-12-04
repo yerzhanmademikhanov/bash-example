a50556b0951ce4c82b64cc380e532ba7e9cd7694
Yerzhan Mademikhanov <yerzhanmademikhanov@yerzhanmademikhanov-mac-c02x36w0jgh6.corp.improbable.io>
2018-12-04 13:25:41 +0000

    redafd

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
-apt-get install golang-go
+add-apt-repository ppa:longsleep/golang-backports
+apt-get update
+apt-get install golang-1.10-go
 
 go run /source/go/src/improbable.io/cmd/imp-ci/main.go events new --name "subcommand-1" --child-of "command"
 sleep 7

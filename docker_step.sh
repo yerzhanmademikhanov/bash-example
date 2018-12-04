8b5159a0428269d84aff064140e4338762a2ea63
Yerzhan Mademikhanov <yerzhanmademikhanov@yerzhanmademikhanov-mac-c02x36w0jgh6.corp.improbable.io>
2018-12-04 11:56:05 +0000

    docker running of steps 2dasd

--- docker_step.sh
+++ docker_step.sh
@@ -1,8 +1,8 @@
 BUILD_EVENT_CACHE=${TMPDIR:-"/tmp"}
 
 docker run \
-    --volume="./build.sh:/build.sh" \
+    --volume="/Users/yerzhanmademikhanov/work/temp/bash-example/build.sh:/scripts/build.sh" \
     --volume="/Users/yerzhanmademikhanov/work/temp/imp-ci:/imp-ci-events-bin/imp-ci" \
     -e="BUILD_EVENT_CACHE=/build_event_cache" \
     --volume="${BUILD_EVENT_CACHE}:/build_event_cache" \
-    --rm ubuntu bash build.sh
+    --rm ubuntu bash /scripts/build.sh
